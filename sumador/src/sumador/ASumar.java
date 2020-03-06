cff4331859231847a484dc3518df79bf37d4aa12
cris2818 <cristg75@gmail.com>
2020-03-06 16:33:31 +0100

    Test una cifra

--- sumador/src/sumador/ASumar.java
+++ sumador/src/sumador/ASumar.java
@@ -1,5 +1,30 @@
 package sumador;
 
+/**
+ * Clase para sumar los dígitos que componen un número
+ * @author Cristina
+ * @version: 1.0
+ */
 public class ASumar {
+	
+	int valor;
+	/**
+	 * Constructor de la clase con parámetros
+	 * @param i número a sumar
+	 */
+	public ASumar(int i) {
+		valor = i;
+	}
+
+	/**
+	 * Método para mostrar el resultado de la suma de todos los dígitos que componen un número
+	 * @param cad cadena que contiene el número que se va a sumar
+	 * @return devuelve una cadena donde muestra el número introducido y el resultado de sumar sus dígitos
+	 */
+	public String mostrar(String cad) {
+		 String numero = cad;
+	     return (numero + " = " + valor);
+	}
 
 }
+
--- sumador/test/sumador/TestASumar.java
+++ sumador/test/sumador/TestASumar.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Clase JUnit para realizar los test de pruebas
+ * @author Cristina
+ * @version: 1.0
+ * 
+ */
 public class TestASumar {
 
+	/**
+	 * Test para un número de un sólo dígito
+	 */
 	@Test
-	public void test() {
-		fail("No implementado aun");
-	}
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar(5);
+        String resultado = sumi.mostrar("5");
+        assertEquals("5 = 5",resultado );
+    }
 
 }
