6f0bf7240e90c8ca3625d0f2d2b093957fe7df91
cris2818 <cristg75@gmail.com>
2020-03-07 00:54:12 +0100

    Finalizacion

---
+++ sumador/src/sumador/CTGFunciona.java
@@ -0,0 +1,27 @@
+package sumador;
+
+/**
+ * @author Cristina
+ * @version: 1.0
+ */
+public class CTGFunciona {
+
+	/**
+	 *  Método principal
+	 * @param args no es necesario tener argumentos
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		
+		ASumar num1 = new ASumar(5);
+		System.out.println(num1.mostrar("5"));
+		
+		ASumar num2 = new ASumar(-12);
+		System.out.println(num2.mostrar("-12"));
+		
+		ASumar num3 = new ASumar(3433);
+		System.out.println(num3.mostrar("3433"));
+
+	}
+
+}
